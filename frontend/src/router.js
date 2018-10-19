d6c8df722747f5b9294c4cc24c0cfb9687764b7e
Daren McCulley <daren.mcculley@cerberus.cira>
2018-10-18 23:41:46 -0400

    testing tabs

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import TrackerList from './views/TrackerList.vue'
+import Test from './views/Test.vue'
 
 Vue.use(Router)
 
@@ -27,6 +28,11 @@ export default new Router({
       path: '/',
       name: 'DMinimal',
       component: TrackerList,
+    },
+    {
+      path: '/test',
+      name: 'test',
+      component: Test,
     },
   ],
 })
---
+++ frontend/src/views/Test.vue
@@ -0,0 +1,107 @@
+<template>
+  <v-container>
+
+    <v-tabs v-model="activeTab"
+            color="cyan"
+            dark
+            slider-color="yellow">
+      <v-tab v-for="n in tabs.length"
+             :key="'tab' + n"
+             ripple>
+        <span>{{ tabs[n - 1].name }}</span>
+        <v-icon class="ml-3"
+                @click.stop="remove(n)">close</v-icon>
+      </v-tab>
+      <v-tab-item v-for="n in tabs.length"
+                  :key="'item' + n">
+        <v-card flat>
+          <v-card-text>{{ tabs[n - 1].content }}</v-card-text>
+        </v-card>
+      </v-tab-item>
+    </v-tabs>
+
+    <div class="text-xs-center mt-3">
+      <v-btn @click="next">next tab</v-btn>
+      <v-btn @click="add">add tab</v-btn>
+    </div>
+
+    <pre>activeTab: {{ activeTab }}</pre>
+    <pre>tabs: {{ tabs }}</pre>
+
+    <v-divider class="my-3"></v-divider>
+
+    <h2 class="mb-2">Better?</h2>
+
+    <v-tabs v-model="activeTab"
+            color="cyan"
+            dark
+            slider-color="yellow">
+      <v-tab v-for="n in tabs.length"
+             :key="'tab' + n"
+             ripple>
+        <span>{{ tabs[n - 1].name }}</span>
+      </v-tab>
+      <v-tab-item v-for="n in tabs.length"
+                  :key="'item' + n">
+        <v-card flat>
+          <v-layout justify-end>
+            <v-icon class="ma-3"
+                  @click.stop="remove(n)">close</v-icon>
+          </v-layout>
+          <v-card-text>{{ tabs[n - 1].content }}</v-card-text>
+        </v-card>
+      </v-tab-item>
+    </v-tabs>
+
+    <div class="text-xs-center mt-3">
+      <v-btn @click="next">next tab</v-btn>
+      <v-btn @click="add">add tab</v-btn>
+    </div>
+
+    <pre>activeTab: {{ activeTab }}</pre>
+    <pre>tabs: {{ tabs }}</pre>
+
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tabIndex: 4,
+      tabs: [
+        { name: 'tab_1', content: 'TAB 1 CONTENT' },
+        { name: 'tab_2', content: 'TAB 2 CONTENT' },
+        { name: 'tab_3', content: 'TAB 3 CONTENT' },
+      ],
+      activeTab: 0,
+    }
+  },
+  methods: {
+    next() {
+      const current = parseInt(this.activeTab)
+      this.activeTab = current < this.tabs.length - 1 ? current + 1 : 0
+    },
+
+    add() {
+      this.tabs.push({
+        name: `tab_${this.tabIndex}`,
+        content: `TAB ${this.tabIndex} CONTENT`,
+      })
+      this.tabIndex += 1
+    },
+
+    remove(n) {
+      this.tabs.splice(n - 1, 1)
+      
+      // this isn't necessary in the second implementation
+      if (n === 1) {
+        this.activeTab = 0
+      }
+    },
+  },
+}
+</script>
+
+<style scoped>
+</style>
